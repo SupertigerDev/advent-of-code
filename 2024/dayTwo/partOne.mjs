bcb3c21f2ee5605dd0786c4c28394075057f1a8c
Supertiger <saawalkhn@googlemail.com>
2024-12-02 21:45:42 +0000

    Complete 2024 Day Two

---
+++ 2024/dayTwo/partOne.mjs
@@ -0,0 +1,31 @@
+import fs from "fs/promises";
+import { report } from "process";
+
+const input = await fs.readFile("./input.txt", "utf-8");
+const lines = input.split(/\r\n|\r|\n/);
+
+const reports = lines.map((line) => line.split(" "));
+
+const safeReports = reports.filter((levels) => {
+  let state;
+
+  for (let i = 0; i < levels.length; i++) {
+    const level = parseInt(levels[i]);
+    const nextLevel = parseInt(levels[i + 1]);
+    if (isNaN(nextLevel)) continue;
+    const newState = nextLevel > level ? "increase" : "decrease";
+    if (!state) {
+      state = newState;
+    }
+    if (newState !== state) {
+      return false;
+    }
+    const difference = Math.abs(nextLevel - level);
+    if (!difference || difference > 3) {
+      return false;
+    }
+  }
+  return true;
+});
+
+console.log(safeReports.length, "Safe Reports.");
